3563d9886d6586539f9828bfbf33897eb091b85c
Adillapramudyarajasa <91449144+Adillapramudyarajasa@users.noreply.github.com>
2021-09-27 14:10:21 +0700

    Create adilla pramudya rajasa- 064002100018- tugas 1 pratekum 2.py

---
+++ Praktikum 2/adilla pramudya rajasa- 064002100018- tugas 1 pratekum 2.py
@@ -0,0 +1,37 @@
+Nama Adilla Pramudya Rajasa
+nim 06400210018
+@author: ASUS
+"""
+
+"""
+"""
+
+
+import math
+
+
+a = float(input("masukan nilai a = "))
+b = float(input("masukan nilai b = "))
+
+print("ini penjumlahan menggunakan math module : ",math.pow(a, b))
+
+hasil  = a + b
+print("bilangan a tambah b adalah",hasil)
+
+hasil = a - b
+print("bilangan a kurang b adalah",abs(hasil))
+
+hasil = a * b
+print("bilangan a dikali b adalah",hasil)
+
+hasil = a / b
+print("bilangan a dibagi b adalah",hasil)
+
+hasil = a % b
+print("jumlah sisa pembagian a dan b adalah",hasil)
+
+hasil = math.log10(a)
+print("bilangan log a dan b adalah",hasil)
+
+hasil = a ** b
+print("3bilangan a pangkat b adalah" , hasil)
