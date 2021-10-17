a935567e928f898bd8fd721e4dc85b1a805a1ff0
Adillapramudyarajasa <91449144+Adillapramudyarajasa@users.noreply.github.com>
2021-10-17 18:15:17 +0700

    Create Adilla Pramudya Rajasa-064002100018-tugas 1 pratikum 4.py

---
+++ Praktikum Algoritma -4/Adilla Pramudya Rajasa-064002100018-tugas 1 pratikum 4.py
@@ -0,0 +1,8 @@
+a = int(input("masukan angka : "))
+print("===============================================")
+while a > 0:
+    for i in range(a):
+        print(a , end="")
+    print()
+    a -=1                        
+print("===============================================")
